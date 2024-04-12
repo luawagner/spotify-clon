d8fb9352e5527a6a7d3bd311e788a0b55b315c08
ludmila wagner <lu.wagner91@gmail.com>
2024-04-12 17:32:38 -0300

    "componente Player reproduciendo audio"

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef, useEffect } from 'react'
 
 export const Pause = () => (
     <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M2.7 1a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7H2.7zm8 0a.7.7 0 0 0-.7.7v12.6a.7.7 0 0 0 .7.7h2.6a.7.7 0 0 0 .7-.7V1.7a.7.7 0 0 0-.7-.7h-2.6z"></path></svg>
@@ -7,9 +7,30 @@ export const Play = () => (
     <svg role="img" height="16" width="16" aria-hidden="true" viewBox="0 0 16 16"><path d="M3 1.713a.7.7 0 0 1 1.05-.607l10.89 6.288a.7.7 0 0 1 0 1.212L4.05 14.894A.7.7 0 0 1 3 14.288V1.713z"></path></svg>
     )
 
+
+
 export function Player () {
     const [isPlaying, setIsPlaying] = useState(false);
+const [currentSong, setCurrentSong] = useState(null);
+const audioRef = useRef() //No olvidar agregar el elemento audio al final del HTML
 
+useEffect(() => {
+    audioRef.current.src = `/music/1/01.mp3`
+}, []) //Establece la url del audio al renderizarse el componente
+
+const handleClick = () => {
+    if (isPlaying) {
+        audioRef.current.pause() //Esto pausa el audio y también actualiza su estado interno 
+        //para que se recuerde la posición en la que se detuvo.
+    } else {
+        audioRef.current.play()
+        // el audio se reanuda desde la posición en la que se detuvo, 
+        //porque esa posición se mantuvo en el estado interno del elemento de audio.
+
+
+    }
+    setIsPlaying(!isPlaying)
+}
 return (
     <div className="flex flex-row justify-between w-full px-4 z-50">
 
@@ -18,7 +39,7 @@ return (
         </div>
         <div className="grid place-content-center gap-4 flex-1">
             <div className="flex justify-center">
-                <button className="bg-white rounded-full p-2" onClick={() => setIsPlaying(!isPlaying)}>
+                <button className="bg-white rounded-full p-2" onClick={handleClick}>
                     {isPlaying? <Pause /> : <Play />}
                 </button>
             </div>
@@ -27,6 +48,8 @@ return (
         <div>
         Volumen...
         </div>
+        <audio ref={audioRef} /> 
+        
     </div>
 )
 
